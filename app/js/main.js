a19a5283cfe9c9deb6ce061e1f3516a4397c7abc
Maxnx <helicopter0760@gmail.com>
2022-08-02 16:18:06 +0300

    added adaptive form

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,18 @@
 $(function () {
-  
+
   $('.tabs__btn').on('click', function (e) {
     e.preventDefault();
     $('.tabs__btn').removeClass('tabs__btn--active');
-    $(this).addClass('tabs__btn--active'); 
-
+    $(this).addClass('tabs__btn--active');
     $('.tabs__content-item').removeClass('tabs__content-item--active');
     $($(this).attr('href')).addClass('tabs__content-item--active');
   });
 
+  $('.tabs__btn').on('click', function () { 
+    $('.tabs__btn').toggleClass('tabs__btn--dropdown');
+     
+  });
+
 });
+
+ 
